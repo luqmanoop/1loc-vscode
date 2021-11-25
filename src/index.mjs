289721c6c2354672b02a6bad3393043d4849215c
Luqman Olushi <olushilukmon03@gmail.com>
2021-11-25 17:02:57 +0100

    build extension

--- src/index.mjs
+++ src/index.mjs
@@ -5,14 +5,17 @@ import {
 } from "./utils.mjs";
 
 const SNIPPETS_PATH_PATTERN = "snippets/**/*.md";
-const ROOT_SNIPPETS_DIR = `${process.cwd()}/1loc/snippets`;
+const ROOT_SNIPPETS_DIR = `${process.cwd()}/extension/snippets`;
 const JS_1LOC_SNIPPETS_PATH = `${ROOT_SNIPPETS_DIR}/1loc.code-snippets`;
 const TS_1LOC_SNIPPETS_PATH = `${ROOT_SNIPPETS_DIR}/1loc-ts.code-snippets`;
 
-getSnippetsPath(SNIPPETS_PATH_PATTERN)
-  .then((snippetPaths) => {
+(async () => {
+  try {
+    const snippetPaths = await getSnippetsPath(SNIPPETS_PATH_PATTERN);
     const [jsSnippets, tsSnippets] = getLanguageSnippets(snippetPaths);
     writeSnippetToFile(JS_1LOC_SNIPPETS_PATH, jsSnippets);
     writeSnippetToFile(TS_1LOC_SNIPPETS_PATH, tsSnippets);
-  })
-  .catch(console.log);
+  } catch (error) {
+    console.log(error);
+  }
+})();
